82329bb69c9d57cc6ffcd43aafff47e93e69e208
Dominik <meyerd@mytum.de>
2016-01-25 15:12:11 +0100

    - add output file writer for winton stock market challenge - added output of leastsquares for stock market challenge

--- brewPipe/output/winton.py
+++ brewPipe/output/winton.py
@@ -1,4 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__ = 'Martin Kiechle <martin.kiechle@gmail.com>'
+import os
+from ..pipelineState import PipelineStateInterface
+
+__author__ = 'Dominik Meyer <meyerd@mytum.de>'
+
+
+class WintonStockDataOutput(PipelineStateInterface):
+    """
+    Output the fitted data in the winton stock market
+    challenge output format.
+    """
+    def __init__(self, output_file, overwrite=False):
+        """
+        Initialize the output writer
+        :param output_file: The path to the output .csv file to
+            be written.
+        :param overwrite: Overwrite the output file if it already
+            exists.
+        """
+        super(WintonStockDataOutput, self).__init__()
+
+        self._output_file = output_file
+        self._overwrite = overwrite
+
+        if os.path.exists(self._output_file) and not self._overwrite:
+            raise RuntimeError("output '%s' already exists" % \
+                               self._output_file)
+
+    def set_data(self, data):
+        """
+        Set the data to be output. The data has to be in the
+        following shape:
+
+        `data: BrewPipeDataFrame (n_samples x prediction_size)`
+
+        where the `prediction_size` is usually 62 and `n_samples`
+        120000.
+        """
+        self._data = data.data
+        self._n_samples = self._data.shape[0]
+        self._prediction_size = self._data.shape[1]
+
+    def write(self):
+        with open(self._output_file, 'w') as f:
+            print >>f, "Id,Predicted"
+            for i in xrange(self._n_samples):
+                for j in xrange(self._prediction_size):
+                    print >>f, "%i_%i,%f" % (i+1, j+1, self._data[i,j])
+
+        return True
--- scratchpad/run_leastsquares_winton.py
+++ scratchpad/run_leastsquares_winton.py
@@ -5,8 +5,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from brewPipe.data.winton import WintonStockData
+from brewPipe.data import BrewPipeDataFrame
+from brewPipe.output.winton import WintonStockDataOutput
 from brewPipe.preprocess.numpy_null import NumpyNullPreprocessor
 from brewPipe.models.tf_leastsquares import TensorflowLeastSquares
+
 
 __author__ = 'Dominik Meyer <meyerd@mytum.de>'
 
@@ -24,16 +27,44 @@ if __name__ == '__main__':
     y = preproc_y.preprocess(y)
 
     lsq = TensorflowLeastSquares(x.data.shape[1], y.data.shape[1],
-                                 learn_rate=0.1, batch_size=10)
+                                 learn_rate=0.1, batch_size=1000,
+                                 silent=False)
     lsq.set_data(x, y)
-    lsq.run(max_steps=1000)
+    lsq.run(max_steps=1000000)
 
     result = lsq.apply_model(x)
     errors = np.sum(np.abs(result.data - y.data), axis=1)
 
-    # plot
-    plt.figure()
-    plt.plot(np.arange(100), errors[:100])
-    plt.xlabel("sample")
-    plt.ylabel("error")
-    plt.show()
+    # # plot
+    # plt.figure()
+    # plt.plot(np.arange(100), errors[:100])
+    # plt.xlabel("sample")
+    # plt.ylabel("error")
+    # plt.show()
+
+    winton_testing = WintonStockData(data_directory="data/", intermediate_directory="intermediates/",
+                                     data_source="test")
+
+    x1 = winton_testing.intraday_2_120()
+    x2 = winton_testing.returns_last_days()
+
+    preproc_x1 = NumpyNullPreprocessor()
+    preproc_x2 = NumpyNullPreprocessor()
+    x1 = preproc_x1.preprocess(x1)
+    x2 = preproc_x1.preprocess(x2)
+
+    # apply model to generate 121 - 180 data
+    y1 = lsq.apply_model(x1)
+    n_samples = y1.data.shape[0]
+
+    # fill the rest of the data with zeros
+    y2 = np.zeros((n_samples, 2))
+
+    y = np.hstack([y1.data, y2])
+    yframe = BrewPipeDataFrame('winton_output')
+    yframe.data = y
+
+    out = WintonStockDataOutput(output_file="submission_winton.csv",
+                                overwrite=True)
+    out.set_data(yframe)
+    out.write()
